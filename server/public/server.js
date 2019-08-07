67b2e9137461e07083d585fe784a1c75d912d698
smbatpoghosyan <smbat.poghosyan@polymorphic.io>
2019-08-07 16:02:12 +0400

    sdfdsf

--- server/public/server.js
+++ server/public/server.js
@@ -9,38 +9,43 @@ const MongoClient = require("mongodb").MongoClient;
 const client = new MongoClient("mongodb+srv://asd:asdasd@cluster0-rdame.mongodb.net/myDb?retryWrites=true&w=majority",{ useNewUrlParser: true});
 client.connect(err => {
 });
-const bodyParser = require("body-parser");
-const dbConfig = require("../config/database.config");
-const branches = require("../routes/branch.routes");
-const playlists = require("../routes/playlist.routes");
-const storage = multer.diskStorage({
-  destination: function(req, file, cb) {
-    cb(null, __dirname + "/../../storage/files");
-  },
-  filename: function(req, file, cb) {
-    cb(null, file.originalname);
-  }
+const conn = mongoose.connection;
+conn.on('error', console.error.bind(console, 'connection error:'));
+conn.once('open', function() {
+  const bodyParser = require("body-parser");
+  const dbConfig = require("../config/database.config");
+  const branches = require("../routes/branch.routes");
+  const playlists = require("../routes/playlist.routes");
+  const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+      cb(null, __dirname + "/../../storage/files");
+    },
+    filename: function(req, file, cb) {
+      cb(null, file.originalname);
+    }
+  });
+  const upload = multer({ storage });
+  let app = express();
+  app.use(cors());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+
+  app.use(express.static(path.join(__dirname, "../../storage")));
+  app.use("/branches", branches);
+  app.use("/playlists", playlists);
+
+  app.post("/upload", upload.single("file"), (req, res) => {
+    if (req.file) {
+      res.json(req.file);
+    } else throw "error";
+  });
+
+  const port = process.env.PORT;
+
+  app.get("/", (req, res) => {
+    res.send("Hiiii");
+  }); // we're connected!
 });
-const upload = multer({ storage });
-let app = express();
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, "../../storage")));
-app.use("/branches", branches);
-app.use("/playlists", playlists);
-
-app.post("/upload", upload.single("file"), (req, res) => {
-  if (req.file) {
-    res.json(req.file);
-  } else throw "error";
-});
-
-const port = process.env.PORT;
-
-app.get("/", (req, res) => {
-  res.send("Hiiii");
-});
 
 app.listen(port, _ => console.log(`The server is listening on port ${port}`));
